8aeebbc391b37bb3afec4a2bf52827cafdd203c4
gupta <saurabh4kaj@gmail.com>
2024-01-10 20:01:58 +0530

    done all routes

---
+++ backend/db.js
@@ -0,0 +1,7 @@
+const mongoose= require('mongoose')
+
+const connection = mongoose.connect('mongodb+srv://saurabh:saurabh@cluster0.hovcp.mongodb.net/kryzen?retryWrites=true&w=majority')
+
+module.exports={
+	connection
+}
